202ec0e239fb604b40e663acd288ce0f5dd62cbd
meddaz <40867923+meddaz@users.noreply.github.com>
2021-01-01 19:59:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function search() {
+
+    var searchTerms = document.getElementById('searchInput').value;
+    var url = "https://www.googleapis.com/books/v1/volumes?maxResults=25&q=" + searchTerms;
+    $.getJSON(url, function (data) {
+
+        console.log(data);
+        var items = data.items;
+        for (var i = 0; i < items.length; i++) {
+          
+
+             display.innerHTML +=  `<div class = "result"><img src="${items[i].volumeInfo.imageLinks.smallThumbnail}">
+                
+               <u> ${items[i].volumeInfo.title} </u><br> ${items[i].volumeInfo.authors} <br> ${items[i].searchInfo.textSnippet}<br> <a href="${items[i].volumeInfo.infoLink}">Link to buy</a></div><br><br><br>`
+
+        }
+    });
+
+}
